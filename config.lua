1d7e45a9e1ca3f0dea180ba2dc0a55e738f22a41
Carlos Galan Cladera <cgalancladera@twilio.com>
2022-04-21 11:00:00 +0200

    refactor(config): disable format on save
    Disable format on save on general config.

--- config.lua
+++ config.lua
@@ -10,7 +10,7 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.colorscheme = "nord"
 -- lvim.colorscheme = "darkplus"
 -- lvim.colorscheme = "darcula"
