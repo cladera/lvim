be1f36024a30d744f04a2fa93d78053610cc328a
Carlos Galan Cladera <cgalancladera@twilio.com>
2022-07-24 08:49:58 +0200

    refactor: improve dap ui configuration

--- lua/user/dap/ui.lua
+++ lua/user/dap/ui.lua
@@ -2,71 +2,74 @@ local dap_ok, dap = pcall(require, "dap")
 local dapui_ok, dapui = pcall(require, "dapui")
 
 if not dap_ok then
-    print('dap not available')
-    return
+  print('dap not available')
+  return
 end
 
 if not dapui_ok then
-    print('dapui not available')
-    return
+  print('dapui not available')
+  return
 end
 
 dapui.setup({
-    icons = { expanded = "▾", collapsed = "▸" },
-    mappings = {
-      -- Use a table to apply multiple mappings
-      expand = { "<CR>", "<2-LeftMouse>" },
-      open = "o",
-      remove = "d",
-      edit = "e",
-      repl = "r",
-      toggle = "t",
-    },
-    -- Expand lines larger than the window
-    -- Requires >= 0.7
-    expand_lines = vim.fn.has("nvim-0.7"),
-    sidebar = {
-      -- You can change the order of elements in the sidebar
-      elements = {-- Provide as ID strings or tables with "id" and "size" keys
-          {
-            id = "scopes",
-            size = 0.25, -- Can be float or integer > 1
-          },
-          { id = "breakpoints", size = 0.25 },
-          -- { id = "stacks", size = 0.25 },
-          -- { id = "watches", size = 00.25 },
-        },
-      size = 40,
-      position = "left", -- Can be "left", "right", "top", "bottom"
-    },
-    tray = {
-      elements = { "repl" },
-      size = 10,
-      position = "bottom", -- Can be "left", "right", "top", "bottom"
-    },
-    floating = {
-      max_height = nil, -- These can be integers or a float between 0 and 1.
-      max_width = nil, -- Floats will be treated as percentage of your screen.
-      border = "single", -- Border style. Can be "single", "double" or "rounded"
-      mappings = {
-        close = { "q", "<Esc>" },
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has("nvim-0.8"),
+  layouts = {
+    {
+      elements = {
+        'scopes',
+        'breakpoints',
+        'stacks',
+        'watches'
       },
+      size = 40,
+      position = 'left'
     },
-    windows = { indent = 1 },
-    render = { 
-      max_type_length = nil, -- Can be integer or nil.
+    {
+      elements = {
+        'repl',
+        'console'
+      },
+      size = 10,
+      position = 'bottom'
+
     }
+
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+  }
 })
 
 -- dap listeners
-dap.listeners.after.event_initialized['dapui-config'] = function ()
+dap.listeners.after.event_initialized['dapui-config'] = function()
   dapui.open()
 end
 
-dap.listeners.after.event_terminated['dapui-config'] = function ()
+dap.listeners.after.event_terminated['dapui-config'] = function()
   dapui.close()
 end
 
-dap.listeners.after.event_exited['dapui-config'] = function ()
+dap.listeners.after.event_exited['dapui-config'] = function()
   dapui.close()
 end
