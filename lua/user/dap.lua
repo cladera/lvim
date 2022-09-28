2ed36553fea828035315ec4d4dd45ba5ab2a1f5c
Carlos Galan Cladera <cgalancladera@twilio.com>
2022-09-28 16:28:54 +0200

    feat: nx typescript debugging

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,29 +1,58 @@
-local ok, dap = pcall(require, "dap")
+local dap_ok, dap = pcall(require, "dap")
+local mason_ok, mason = pcall(require, "mason-registry")
 
-if ok then
-  dap.configurations.java = {
-    {
-      type = "java";
-      request = "attach";
-      name = "Java Attach";
-      hostName = "localhost";
-      port = 5005;
-    },
+if not dap_ok then
+  return
+end
+
+if mason_ok and mason.is_installed("node-debug2-adapter") then
+  dap.adapters.node2 = {
+    type = 'executable',
+    command = 'node',
+    args = { vim.fn.stdpath("data") .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' },
   }
 
-  require('dap.ext.vscode').load_launchjs()
+  local function resolve_nx_jest_config()
+    local currentBufferPath = vim.fn.fnamemodify(vim.fn.expand("%"), ":h")
+    local sourcePathPos = vim.fn.stridx(currentBufferPath, "/src")
+    if sourcePathPos < 0 then
+      sourcePathPos = vim.fn.stridx(currentBufferPath, "/tests")
+    end
+    local projectRoot = vim.fn.strpart(currentBufferPath, 0, sourcePathPos)
+
+    return projectRoot .. "/jest.config.ts"
+  end
+
+  local function resolve_nx_jest_args()
+    return { "${fileBasenameNoExtension}", '--config', resolve_nx_jest_config() }
+  end
+
+  local function resolve_jest_program()
+    return vim.fn.getcwd() .. "/node_modules/.bin/jest"
+  end
+
+  dap.configurations.typescript = {
+    {
+      type = "node2",
+      name = "NX: Jest current file",
+      request = "launch",
+      program = resolve_jest_program,
+      args = resolve_nx_jest_args,
+      cwd = vim.fn.getcwd,
+      console = 'integratedTerminal'
+    }
+  }
 end
+
 function resolve_main_class()
   local currentBuffer = vim.fn.fnamemodify(vim.fn.expand("%"), ":r")
   local packageAnchor = "main/java/"
   local packageStart = vim.fn.strridx(currentBuffer, packageAnchor) + vim.fn.strlen(packageAnchor)
   local packagePath = vim.fn.strpart(currentBuffer, packageStart)
-  local mainClass = string.gsub(packagePath, "/", ".")
 
-  print(mainClass)
+  return string.gsub(packagePath, "/", ".")
 
-  return mainClass
-  end
+end
 
 dap.configurations.java = {
   {
@@ -48,16 +77,6 @@ dap.configurations.java = {
   },
 }
 
-dap.configurations.typescript = {
-  {
-    name = 'Launch',
-    type = 'node2',
-    request = 'launch',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-}
-
+require('dap.ext.vscode').load_launchjs(vim.fn.getcwd() .. "/.vscode/launch.json", {
+  node2 = { 'javascript', 'typescript' }
+})
