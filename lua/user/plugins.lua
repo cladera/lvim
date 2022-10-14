2538a3eb4cb5c4c3900d36f9ea58ac68b91a3bd8
Carlos Galan Cladera <cgalancladera@twilio.com>
2022-10-14 09:49:23 +0200

    refactor(plugins): update plugins list

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -7,23 +7,10 @@ lvim.plugins = {
   {'arcticicestudio/nord-vim'},
   { 'tpope/vim-fugitive' },
   { 'tpope/vim-surround' },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    -- event = "BufReadPre",
-    config = function()
-      require("user.blankline").config()
-    end,
-  },
   { 'editorconfig/editorconfig-vim' },
   {
     "ThePrimeagen/harpoon",
   },
-  -- {
-  --   "folke/todo-comments.nvim",
-  --   config = function()
-  --     require("user.todo_comments").config()
-  --   end,
-  -- },
   {
     "iamcco/markdown-preview.nvim",
     run = "cd app && npm install",
@@ -31,9 +18,7 @@ lvim.plugins = {
   },
   { "mfussenegger/nvim-jdtls" },
   { "nvim-telescope/telescope-ui-select.nvim" },
-  { "rcarriga/nvim-dap-ui" },
   { "doums/darcula" },
-  -- { "folke/tokyonight.nvim" },
   { "catppuccin/nvim" },
   { "lunarvim/darkplus.nvim" },
   { "rose-pine/neovim" },
