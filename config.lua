26fc805f8a08680d3b890789092b719227d0cd7f
Carlos Galan Cladera <carlos.galancladera@okta.com>
2023-07-25 14:56:24 +0200

    refactor: disable system clipboard binding

--- config.lua
+++ config.lua
@@ -49,6 +49,7 @@ require "user.lazygit"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "100"
+vim.opt.clipboard = ""
 
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
